db3b17a8eea70f1a2c843955304e543ab4b70c8d
PozzaMarco <marco.pozza.3@studenti.unipd.it>
2020-04-14 16:57:07 +0200

    update function

--- typescript_files/minHeap.ts
+++ typescript_files/minHeap.ts
@@ -13,21 +13,24 @@ import HeapNode from "./heapNode";
 
 export default class MinHeap {
   heap: HeapNode[];
+  areInHeap: Map<number, boolean>//TODO Al posto di boolean tenere l'indice nell'array!
 
   constructor() {
     this.heap = []; //Set first element (position 0) to null to simplify the calcs
+    this.areInHeap = new Map();
   }
 
   getMin(): HeapNode {
     return this.heap[0];
   }
 
-  insert(node: HeapNode) {
+  insert(node: HeapNode){
     this.heap.push(node);
-    this.heapifyUp();
+    this.heapifyUp()
+    this.areInHeap.set(node.node, true);
   }
 
-  heapifyUp() {
+  heapifyUp(){
     let index = this.heap.length - 1; // index of last element
 
     while (index > 0) {
@@ -36,24 +39,26 @@ export default class MinHeap {
         parentIndex = Math.floor((index - 1) / 2),
         parent = this.heap[parentIndex]; //take parent of last element
 
-      if (parent.value <= element.value) break; // if parent is less or equal then element nothing to do
+      if (parent.weight <= element.weight) break; // if parent is less or equal then element nothing to do
 
+      //this.areInHeap.set(this.heap[parentIndex].node, index)
       this.heap[index] = parent; // else i have to swap it up the tree
       this.heap[parentIndex] = element;
       index = parentIndex;
     }
+    return index;
   }
 
   extractMin(): HeapNode {
     let min = this.heap[0]; // save minimum value
     
     let lastElement = this.heap.pop();
+    this.areInHeap.delete(min.node);
 
     if(this.heap.length != 0){
       this.heap[0] = lastElement; // remove last value and put at first place
       this.heapifyDown(0); // find the right position for the new first node
     }
-
     return min;
   }
 
@@ -64,11 +69,11 @@ export default class MinHeap {
     const length = this.heap.length;
 
     // if left child is lower than parent
-    if (left <= length && this.heap[left] && this.heap[left].value < this.heap[smallest].value) {
+    if (left <= length && this.heap[left] && this.heap[left].weight < this.heap[smallest].weight) {
       smallest = left;
     }
     // if right child is lower than parent
-    if (right <= length && this.heap[right] && this.heap[right].value < this.heap[smallest].value) {
+    if (right <= length && this.heap[right] && this.heap[right].weight < this.heap[smallest].weight) {
       smallest = right;
     }
     // Swap
@@ -80,29 +85,18 @@ export default class MinHeap {
       this.heapifyDown(smallest);
     }
   }
-  
+
   contains(node: number): boolean{
-    return this.containsUtil(node, 0);
+    return this.areInHeap.has(node);
   }
 
-  containsUtil(node: number, index: number): boolean {
-    if(node == this.heap[index].value)
-      return true;
-    else{
-      let left = 2 * index + 1;
-      let right = 2 * index + 2;
-
-      if(this.heap[left] && node >= this.heap[left].value && this.heap[right] && node >= this.heap[right].value){
-        return this.containsUtil(node, left) || this.containsUtil(node, right);
+  update(node: number, weight: number){
+    for(let index = 0; index < this.heap.length; index++){
+      if(this.heap[index].node == node){
+        this.heap.splice(index,1);
+        this.insert(new HeapNode(node, weight));
+        break;
       }
-      else if(this.heap[left] && node >= this.heap[left].value){
-        return this.containsUtil(node,left);
-      }
-      else if(this.heap[right] && node >= this.heap[right].value)
-        return this.containsUtil(node, right);
-      else
-        return false;
-
     }
   }
 
