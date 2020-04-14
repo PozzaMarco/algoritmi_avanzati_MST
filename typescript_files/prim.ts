f3e31c3312ee10c70223b12070b0c9f959130a4b
PozzaMarco <marco.pozza.3@studenti.unipd.it>
2020-04-14 18:00:18 +0200

    Optimization completed

--- typescript_files/prim.ts
+++ typescript_files/prim.ts
@@ -1,12 +1,12 @@
 import Graph from "./graph";
-import Edge from "./edge";
 import MinHeap from "./minHeap";
+import HeapNode from "./heapNode";
 
 function initialization(graph: Graph): any{
-    let key: number[];
-    let parents: number[];
+    let key: number[] = [];
+    let parents: number[] = [];
        
-    for(let index = 0; index < graph.getNumberOfNodes(), index++){
+    for(let index = 1; index <= graph.getNumberOfNodes(); index++){
         key[index] = Infinity;
         parents[index] = null;
     }
@@ -14,9 +14,14 @@ function initialization(graph: Graph): any{
 }
 
 function createPriorityQueue(graph: Graph, key: number[]): MinHeap{
-    return new MinHeap();
+    let minHeap = new MinHeap();
+
+    for(let index = 1; index <= graph.getNumberOfNodes(); index++)
+        minHeap.insert(new HeapNode(index, key[index]));
+
+    return minHeap;
 }
-function prim(graph: Graph, startingNode: number): Array<Edge>{
+function prim(graph: Graph, startingNode: number): any{
     let key: number[];// Peso minimo di un qualsiasi lato che connette un nodo all'albero
     let parents: number[];// Rappresenta il padre del nodo nell'albero
     
@@ -24,4 +29,20 @@ function prim(graph: Graph, startingNode: number): Array<Edge>{
     key[startingNode] = 0;
 
     let priorityQueue = createPriorityQueue(graph, key);
+
+    while(!priorityQueue.isEmpty()){
+        let current = priorityQueue.extractMin();
+        let adjacentOfCurrent = graph.getAdjacentNodesOf(current.node);
+
+        adjacentOfCurrent.forEach(node =>{
+            if(priorityQueue.contains(node) && graph.weightBetween(current.getNode(), node) < key[node]){
+                parents[node] = current.getNode();
+                key[node] = graph.weightBetween(current.getNode(), node);
+                priorityQueue.update(node, key[node]);
+            }
+        })
+    }
+    return [key, parents]
 }
+
+export default prim;
